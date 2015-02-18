a8d6a1ada3c97c8bbaba8af2b93fb0356bd92ed4
Vincent Untz <vuntz@suse.com>
2015-02-18 23:54:13 +0100

    fixup! HA: Set Mandatory ordering for Keystone startup

--- chef/cookbooks/keystone/recipes/ha.rb
+++ chef/cookbooks/keystone/recipes/ha.rb
@@ -65,10 +65,10 @@ if node[:keystone][:frontend] == 'native'
     only_if { CrowbarPacemakerHelper.is_cluster_founder?(node) }
   end
 
-  crowbar_pacemaker_order_only_existing "o-#{service_name}" do
-    ordering "postgresql rabbitmq cl-#{service_name}"
-    score "Mandatory"
-    action [ :create ]
+  crowbar_pacemaker_order_only_existing "o-cl-#{service_name}" do
+    ordering [ "postgresql", "rabbitmq", "cl-#{service_name}" ]
+    score "Optional"
+    action :create
     only_if { CrowbarPacemakerHelper.is_cluster_founder?(node) }
   end
 
